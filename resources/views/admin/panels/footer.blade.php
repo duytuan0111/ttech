728ec89186ae7317a8a773fa3d4d4a7a5a6e84a2
Tuân Nguyễn duy <tuannguyenduy@Tuans-MacBook-Pro.local>
2024-01-12 15:21:38 +0700

    update

--- resources/views/admin/panels/footer.blade.php
+++ resources/views/admin/panels/footer.blade.php
@@ -2,7 +2,7 @@
   <div class="pull-right hidden-xs">
     <b>Version</b> 1.0.0
   </div>
-  <strong>Copyright &copy; 2022 <a href="https://fhmvietnam.vn" target="_blank">FHM Việt Nam</a>.</strong> All rights
+  <strong>Copyright &copy; 2022 <a href="javascript:void(0)" target="_blank">TM CREATA</a>.</strong> All rights
   reserved.
 </footer>
 
